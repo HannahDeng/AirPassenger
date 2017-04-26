915ecf56f08e2d6872fa91601f8fb85d70dd6243
Han Deng <han.deng@hotmail.com>
2017-04-26 18:44:37 +0200

    d

---
+++ js/market.js
@@ -0,0 +1,136 @@
+var trace1 = {
+  lat: [34.053717, 41.702014, 42.3604823, 33.7490987, 33.1192875, 39.7391536, 40.7880091, 36.1662859], 
+  latsrc: 'HannahDeng:9:a7f15e', 
+  locationmode: 'USA-states', 
+  lon: [-118.2427266, -87.7785509, -71.0595678, -84.3901849, -97.0255641, -104.9847034, -74.0142761, -115.149225], 
+  lonsrc: 'HannahDeng:9:68757a', 
+  marker: {
+    color: 'rgb(214, 39, 40)', 
+    line: {
+      color: 'rgb(255,255,255)', 
+      width: 1
+    }, 
+    size: [1518.49555556, 1481.91111111, 1086.83111111, 1040.56666667, 991.224444444, 932.951111111, 919.997777778, 810.013333333], 
+    sizemode: 'area', 
+    sizesrc: 'HannahDeng:9:c89c15'
+  }, 
+  name: '(40000, 76000)', 
+  text: ['Los Angeles<br>Passengers: 75924.7777778<br>Destinations: 34', 'Chicago<br>Passengers: 74095.5555556<br>Destinations: 56', 'Boston<br>Passengers: 54341.5555556<br>Destinations: 42', 'Atlanta<br>Passengers: 52028.3333333<br>Destinations: 55', 'Dallas<br>Passengers: 49561.2222222<br>Destinations: 51', 'Denver<br>Passengers: 46647.5555556<br>Destinations: 43', 'New York<br>Passengers: 45999.8888889<br>Destinations: 27', 'Las Vegas<br>Passengers: 40500.6666667<br>Destinations: 37'], 
+  textsrc: 'HannahDeng:9:2c4d47', 
+  type: 'scattergeo'
+};
+var trace2 = {
+  lat: [29.7589382, 46.8171809, 28.5421175, 33.4467681, 38.029306, 30.2711286, 44.9772995, 26.640628, 41.5331066, 37.7792768], 
+  latsrc: 'HannahDeng:9:eedd4f', 
+  locationmode: 'USA-states', 
+  lon: [-95.3676974, -95.8453253, -81.3790462, -112.0756724, -78.4766781, -97.7436995, -93.2654692, -81.8723084, -81.5790137, -122.4192704], 
+  lonsrc: 'HannahDeng:9:73d3c8', 
+  marker: {
+    color: 'rgb(190,41,236)', 
+    line: {
+      color: 'rgb(255,255,255)', 
+      width: 1
+    }, 
+    size: [550.362222222, 451.948888889, 349.424444444, 338.548888889, 332.931111111, 332.495555556, 325.353333333, 251.835555556, 228.308888889, 209.417777778], 
+    sizemode: 'area', 
+    sizesrc: 'HannahDeng:9:68d535'
+  }, 
+  name: '(10000, 40000)', 
+  text: ['Houston<br>Passengers: 27518.1111111<br>Destinations: 34', 'Detroit<br>Passengers: 22597.4444444<br>Destinations: 27', 'Orlando<br>Passengers: 17471.2222222<br>Destinations: 18', 'Phoenix<br>Passengers: 16927.4444444<br>Destinations: 17', 'Charlotte<br>Passengers: 16646.5555556<br>Destinations: 36', 'Austin<br>Passengers: 16624.7777778<br>Destinations: 29', 'Minneapolis<br>Passengers: 16267.6666667<br>Destinations: 20', 'Fort Myers<br>Passengers: 12591.7777778<br>Destinations: 17', 'Cleveland<br>Passengers: 11415.4444444<br>Destinations: 19', 'San Francisco<br>Passengers: 10470.8888889<br>Destinations: 6'], 
+  textsrc: 'HannahDeng:9:991de0', 
+  type: 'scattergeo'
+};
+var trace3 = {
+  lat: [39.0844687, 39.9622601, 39.7683331, 40.4897871, 45.5202471, 29.9499323, 36.187025, 42.8864468, 41.7634935, 42.9105722, 39.1014537, 40.7670126, 35.0841034], 
+  latsrc: 'HannahDeng:9:6170ba', 
+  locationmode: 'USA-states', 
+  lon: [-94.5630298, -83.0007065, -86.1583502, -81.4456706, -122.6741949, -90.0701156, -86.78086153, -78.8783689, -72.6830523, -87.8606367, -84.5124602, -111.8904308, -106.6509851], 
+  lonsrc: 'HannahDeng:9:76b688', 
+  marker: {
+    color: 'rgb(2,137,0)', 
+    line: {
+      color: 'rgb(255,255,255)', 
+      width: 1
+    }, 
+    size: [183.026666667, 164.746666667, 158.644444444, 155.482222222, 152.564444444, 152.56, 136.462222222, 127.248888889, 125.551111111, 116.651111111, 115.346666667, 110.586666667, 104.977777778], 
+    sizemode: 'area', 
+    sizesrc: 'HannahDeng:9:29db8b'
+  }, 
+  name: '(4000, 10000)', 
+  text: ['Kansas City<br>Passengers: 9151.33333333<br>Destinations: 20', 'Columbus<br>Passengers: 8237.33333333<br>Destinations: 18', 'Indianapolis<br>Passengers: 7932.22222222<br>Destinations: 15', 'Philadelphia<br>Passengers: 7774.11111111<br>Destinations: 12', 'Portland<br>Passengers: 7628.22222222<br>Destinations: 10', 'New Orleans<br>Passengers: 7628.0<br>Destinations: 14', 'Nashville<br>Passengers: 6823.11111111<br>Destinations: 14', 'Buffalo<br>Passengers: 6362.44444444<br>Destinations: 12', 'Hartford<br>Passengers: 6277.55555556<br>Destinations: 11', 'Milwaukee<br>Passengers: 5832.55555556<br>Destinations: 11', 'Cincinnati<br>Passengers: 5767.33333333<br>Destinations: 16', 'Salt Lake City<br>Passengers: 5529.33333333<br>Destinations: 7', 'Albuquerque<br>Passengers: 5248.88888889<br>Destinations: 14'], 
+  textsrc: 'HannahDeng:9:1577d7', 
+  type: 'scattergeo'
+};
+var trace4 = {
+  lat: [43.5988375, 47.6038321, 27.9477595, 38.5804609, 33.543682, 35.7804015, 38.368429, 42.9632405, 41.2587317, 38.2856247, 29.4246002, 38.209237, 35.1464797, 39.3642852, 40.4416941, 35.63448665, 33.8345281, 41.5910641, 29.167082, 38.6272733, 48.754402, 38.0500627, 43.016193, 35.4729886, 39.530895], 
+  latsrc: 'HannahDeng:9:3b4ca4', 
+  locationmode: 'USA-states', 
+  lon: [-116.243255, -122.3300624, -82.458444, -121.530234, -86.779633, -78.6390779, -81.69957, -85.6678639, -95.9378732, -85.8241312, -98.4951405, -85.7038502, -90.1845388, -74.4229351, -79.9900861, -120.6702823, -116.5389475, -93.6037149, -81.00565446, -90.1978889, -122.4788602, -85.5457877, -83.705521, -97.5170536, -119.814972], 
+  lonsrc: 'HannahDeng:9:31d42d', 
+  marker: {
+    color: 'rgb(255,87,0)', 
+    line: {
+      color: 'rgb(255,255,255)', 
+      width: 1
+    }, 
+    size: [69.9444444444, 68.8244444444, 60.2733333333, 60.1622222222, 57.1155555556, 56.7355555556, 49.0866666667, 47.18, 47.0977777778, 46.0044444444, 45.1244444444, 41.3688888889, 40.7888888889, 40.0022222222, 35.9755555556, 35.3866666667, 34.8955555556, 33.5155555556, 28.5822222222, 27.9622222222, 26.7422222222, 25.5066666667, 19.0333333333, 18.1755555556, 16.9333333333], 
+    sizemode: 'area', 
+    sizesrc: 'HannahDeng:9:427442'
+  }, 
+  name: '(700, 4000)', 
+  text: ['Boise<br>Passengers: 3497.22222222<br>Destinations: 10', 'Seattle<br>Passengers: 3441.22222222<br>Destinations: 5', 'Tampa<br>Passengers: 3013.66666667<br>Destinations: 1', 'Sacramento<br>Passengers: 3008.11111111<br>Destinations: 4', 'Birmingham<br>Passengers: 2855.77777778<br>Destinations: 10', 'Raleigh<br>Passengers: 2836.77777778<br>Destinations: 7', 'Charleston<br>Passengers: 2454.33333333<br>Destinations: 6', 'Grand Rapids<br>Passengers: 2359.0<br>Destinations: 9', 'Omaha<br>Passengers: 2354.88888889<br>Destinations: 7', 'Albany<br>Passengers: 2300.22222222<br>Destinations: 6', 'San Antonio<br>Passengers: 2256.22222222<br>Destinations: 6', 'Louisville<br>Passengers: 2068.44444444<br>Destinations: 7', 'Memphis<br>Passengers: 2039.44444444<br>Destinations: 7', 'Atlantic City<br>Passengers: 2000.11111111<br>Destinations: 5', 'Pittsburgh<br>Passengers: 1798.77777778<br>Destinations: 5', 'El Paso<br>Passengers: 1769.33333333<br>Destinations: 6', 'Palm Springs<br>Passengers: 1744.77777778<br>Destinations: 3', 'Des Moines<br>Passengers: 1675.77777778<br>Destinations: 7', 'Dayton<br>Passengers: 1429.11111111<br>Destinations: 6', 'St. Louis<br>Passengers: 1398.11111111<br>Destinations: 2', 'Bellingham<br>Passengers: 1337.11111111<br>Destinations: 4', 'Washington<br>Passengers: 1275.33333333<br>Destinations: 1', 'Flint<br>Passengers: 951.666666667<br>Destinations: 4', 'Oklahoma City<br>Passengers: 908.777777778<br>Destinations: 4', 'Reno<br>Passengers: 846.666666667<br>Destinations: 3'], 
+  textsrc: 'HannahDeng:9:f7ae5a', 
+  type: 'scattergeo'
+};
+var trace5 = {
+  lat: [44.4723989, 43.1854754, 46.8749671, 36.7394421, 36.0726355, 34.851354, 26.1906306, 35.2072185, 28.709856, 41.9758872, 40.2663107, 39.191097, 40.3211808, 43.0481221, 40.6297632, 46.8083268, 34.7456275, 44.8011821, 38.0573603, 41.0799898, 35.9603948, 33.8160058, 38.0464066, 32.2217422, 45.5202471, 34.577718, 44.0505054, 27.8002542, 41.1572639, 40.613953, 30.421309, 32.0835407, 36.111508, 43.1030928, 48.2325095, 27.3364347, 38.8339578, 42.3264181, 45.7874957, 44.6928101, 41.6833813, 47.9078244, 29.0338575, 29.0338575, 30.4507462, 43.7452402, 40.666149], 
+  latsrc: 'HannahDeng:9:d0ffa2', 
+  locationmode: 'USA-states', 
+  lon: [-73.2114941, -77.61068605, -96.900362, -119.7848307, -79.7919754, -82.3984882, -97.6961026, -101.8338246, -100.4794168, -91.6704053, -76.8861122, -106.817535, -79.3794811, -76.1474244, -91.3151506, -100.7837392, -92.15314448, -68.7778138, -78.4408438, -85.1386015, -83.9210261, -78.680016, -84.4970393, -110.9264759, -122.6741949, -86.83583561, -123.0950506, -97.3955744, -85.4883072, -75.477791, -87.2169149, -81.0998342, -94.20986672, -79.0302618, -101.2962732, -82.5306527, -104.8253485, -122.8718605, -108.49607, -73.4555973, -86.2500066, -97.0592028, -95.4343859, -95.4343859, -91.154551, -92.9701918, -89.5800978], 
+  lonsrc: 'HannahDeng:9:2c4b58', 
+  marker: {
+    color: 'rgb(0,54,255)', 
+    line: {
+      color: 'rgb(255,255,255)', 
+      width: 1
+    }, 
+    size: [13.7577777778, 12.62, 11.2, 10.5555555556, 10.5111111111, 10.4688888889, 10.0533333333, 9.96666666667, 9.76666666667, 8.72444444444, 8.71777777778, 8.52444444444, 8.35777777778, 7.86, 7.8, 7.58222222222, 7.49555555556, 7.32222222222, 7.04222222222, 6.57777777778, 6.53777777778, 6.49111111111, 6.18, 6.11333333333, 5.97111111111, 5.75777777778, 5.52666666667, 4.92, 4.57111111111, 4.47777777778, 4.12222222222, 3.95555555556, 3.95333333333, 3.76222222222, 3.69777777778, 3.58666666667, 3.58444444444, 3.55555555556, 3.49555555556, 3.48666666667, 3.46888888889, 3.36888888889, 3.30666666667, 3.26888888889, 3.18444444444, 3.18, 3.16888888889], 
+    sizemode: 'area', 
+    sizesrc: 'HannahDeng:9:8ed96c'
+  }, 
+  name: '(158, 700)', 
+  text: ['Burlington<br>Passengers: 687.888888889<br>Destinations: 3', 'Rochester<br>Passengers: 631.0<br>Destinations: 2', 'Fargo<br>Passengers: 560.0<br>Destinations: 2', 'Fresno<br>Passengers: 527.777777778<br>Destinations: 3', 'Greensboro<br>Passengers: 525.555555556<br>Destinations: 2', 'Greenville<br>Passengers: 523.444444444<br>Destinations: 2', 'Harlingen<br>Passengers: 502.666666667<br>Destinations: 2', 'Amarillo<br>Passengers: 498.333333333<br>Destinations: 2', 'Eagle<br>Passengers: 488.333333333<br>Destinations: 2', 'Cedar Rapids<br>Passengers: 436.222222222<br>Destinations: 2', 'Harrisburg<br>Passengers: 435.888888889<br>Destinations: 2', 'Aspen<br>Passengers: 426.222222222<br>Destinations: 2', 'Latrobe<br>Passengers: 417.888888889<br>Destinations: 2', 'Syracuse<br>Passengers: 393.0<br>Destinations: 2', 'Madison<br>Passengers: 390.0<br>Destinations: 2', 'Bismarck<br>Passengers: 379.111111111<br>Destinations: 2', 'Little Rock<br>Passengers: 374.777777778<br>Destinations: 2', 'Bangor<br>Passengers: 366.111111111<br>Destinations: 2', 'Key West<br>Passengers: 352.111111111<br>Destinations: 2', 'Fort Wayne<br>Passengers: 328.888888889<br>Destinations: 2', 'Knoxville<br>Passengers: 326.888888889<br>Destinations: 2', 'Myrtle Beach<br>Passengers: 324.555555556<br>Destinations: 1', 'Lexington<br>Passengers: 309.0<br>Destinations: 2', 'Tucson<br>Passengers: 305.666666667<br>Destinations: 1', 'Portland<br>Passengers: 298.555555556<br>Destinations: 1', 'Huntsville<br>Passengers: 287.888888889<br>Destinations: 1', 'Eugene<br>Passengers: 276.333333333<br>Destinations: 1', 'Corpus Christi<br>Passengers: 246.0<br>Destinations: 1', 'Columbia<br>Passengers: 228.555555556<br>Destinations: 1', 'Allentown<br>Passengers: 223.888888889<br>Destinations: 1', 'Pensacola<br>Passengers: 206.111111111<br>Destinations: 1', 'Savannah<br>Passengers: 197.777777778<br>Destinations: 1', 'Fayetteville<br>Passengers: 197.666666667<br>Destinations: 1', 'Niagara Falls<br>Passengers: 188.111111111<br>Destinations: 1', 'Minot<br>Passengers: 184.888888889<br>Destinations: 1', 'Sarasota<br>Passengers: 179.333333333<br>Destinations: 1', 'Colorado Springs<br>Passengers: 179.222222222<br>Destinations: 1', 'Medford<br>Passengers: 177.777777778<br>Destinations: 1', 'Billings<br>Passengers: 174.777777778<br>Destinations: 1', 'Plattsburgh<br>Passengers: 174.333333333<br>Destinations: 1', 'South Bend<br>Passengers: 173.444444444<br>Destinations: 1', 'Grand Forks<br>Passengers: 168.444444444<br>Destinations: 1', 'Jackson<br>Passengers: 165.333333333<br>Destinations: 1', 'Jackson<br>Passengers: 163.444444444<br>Destinations: 1', 'Baton Rouge<br>Passengers: 159.222222222<br>Destinations: 1', 'Lansing<br>Passengers: 159.0<br>Destinations: 1', 'Peoria<br>Passengers: 158.444444444<br>Destinations: 1'], 
+  textsrc: 'HannahDeng:9:da9e59', 
+  type: 'scattergeo'
+};
+var data = [trace1, trace2, trace3, trace4, trace5];
+var layout = {
+  geo: {
+    countrycolor: 'rgb(255, 255, 255)', 
+    countrywidth: 1, 
+    landcolor: 'rgb(190, 203, 255)', 
+    projection: {type: 'albers usa'}, 
+    scope: 'usa', 
+    showland: true, 
+    subunitcolor: 'rgb(255, 255, 255)', 
+    subunitwidth: 1
+  }, 
+  showlegend: true, 
+  legend: {
+    x: 0.74,
+    y: 0.07
+  },
+  paper_bgcolor: 'rgba(0,0,0,0)',
+  plot_bgcolor: 'rgba(0,0,0,0)',
+  margin: {
+    l: 0,
+    r: 0,
+    b: 0,
+    t: 0,
+    pad: 0
+  },
+};
+Plotly.plot('plotly-div', {
+  data: data,
+  layout: layout
+});
